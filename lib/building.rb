19c3d1ddbb58e2873324928294c47d132231c1e3
Stephanie Friend <stephaniefriend91@gmail.com>
2019-12-11 10:15:32 -0700

    Add test for average rent and highest rent calculations

--- lib/building.rb
+++ lib/building.rb
@@ -1,2 +1,23 @@
 class Building
-end 
+
+  attr_reader :units
+
+  def initialize
+    @units = []
+  end
+
+  def add_unit(new_unit)
+    @units << new_unit
+  end
+
+  # def average_rent
+  #   unit_rent = @units.find_all do |unit|
+  #     unit[:monthly_rent]
+  #   end
+  #   (unit_rent.sum / 2).round(1)
+  # end
+
+  def renter_with_highest_rent
+    
+  end
+end
--- test/building_test.rb
+++ test/building_test.rb
@@ -5,4 +5,67 @@ require './lib/apartment'
 require './lib/building'
 
 class BuildingTest < Minitest::Test
+
+  def test_is_exists
+    building = Building.new
+
+    assert_instance_of Building, building
+  end
+
+  def test_building_starts_with_no_units
+    building = Building.new
+
+    assert_equal [], building.units
+  end
+
+  def test_building_can_add_units
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+
+    assert_equal [unit1, unit2], building.units
+  end
+
+  # def test_it_can_calculate_average_rent
+  #   building = Building.new
+  #   unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+  #   unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+  #
+  #   building.add_unit(unit1)
+  #   building.add_unit(unit2)
+  #
+  #   assert_equal 1099.5, building.average_rent
+  # end
+
+  def test_it_can_show_renter_with_highest_rent
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 3, bedrooms: 2})
+    renter1 = Renter.new("Spencer")
+
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+
+    unit2.add_renter(renter1)
+
+    assert_equal renter1, building.renter_with_highest_rent
+
+    renter2 = Renter.new("Jessie")
+
+    unit1.add_renter(renter2)
+
+    assert_equal renter2, building.renter_with_highest_rent
+
+    renter2 = Renter.new("Max")
+
+    unit3.add_renter(renter3)
+
+    assert_equal renter2, building.renter_with_highest_rent
+  end
 end
