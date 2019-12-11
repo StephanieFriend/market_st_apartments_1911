b8b9c53ce1908949d145fd17989c97a234432c66
Stephanie Friend <stephaniefriend91@gmail.com>
2019-12-11 09:40:53 -0700

    Create test for apartment, building, renter and worked through iteration 1

---
+++ test/renter_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+
+class RenterTest < Minitest::Test
+
+  def test_is_exists
+    renter1 = Renter.new("Jessie")
+
+    assert_instance_of Renter, renter1
+  end
+
+  def test_it_has_attributes
+    renter1 = Renter.new("Jessie")
+
+    assert_equal "Jessie", renter1.name
+  end
+end
